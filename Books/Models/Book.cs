6b15b633881e45157afcfbec5a10fa17c6fdef41
NicoleZi <colle888@aol.com>
2023-09-29 20:51:41 +0200

    Titel Autor Save in Database

--- Books/Models/Book.cs
+++ Books/Models/Book.cs
@@ -11,7 +11,8 @@ namespace Books.Models
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public string Text { get; set; }
-        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Autor {  get; set; }
+        //public DateTime Date { get; set; }
     }
 }
